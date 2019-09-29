058f31e344ef2fb0d247bc53eb54abc1f62eb180
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-29 18:02:24 -0400

    practice string basics, including splicing, in python

---
+++ python_level_one/strings.py
@@ -0,0 +1,6 @@
+mystring = 'abcdefg'
+print(mystring[2:]) # from the 2nd index to the end
+print(mystring[:4]) # up to but not including the 4th index
+print(mystring[::]) # step-size of 1: from the beginning to the end
+print(mystring[::2]) # step-size of 2: every 2nd letter from the beginning (0) to end
+
